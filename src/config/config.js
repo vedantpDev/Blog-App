cdccd98bd022aeeb3d2bd26b46b36bd1438145be
vedantpDev <patilvedant333.com>
2024-02-13 23:24:49 +0530

    set ENV and Appwrite DaaS

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+  appwriteUrl: String(process.env.REACT_APP_APPWRITE_URL),
+  appwriteProjectId: String(process.env.REACT_APP_PROJECT_ID),
+  appwriteDatabaseId: String(process.env.REACT_APP_DATABASE_ID),
+  appwriteCollectionId: String(process.env.REACT_APP_COLLECTION_ID),
+  appwriteBucketId: String(process.env.REACT_APP_BUCKET_ID),
+};
+
+export default config;
