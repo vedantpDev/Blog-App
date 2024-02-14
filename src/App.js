24c53f1d56aed2c965621e36eb7aff9383531d5a
vedantpDev <patilvedant333.com>
2024-02-14 08:15:08 +0530

    appwrite auth services

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import "./App.css";
-
 function App() {
-  console.log(process.env.REACT_APP_APPWRITE_URL);
   return <div>Run app with Appwrite</div>;
 }
 
---
+++ src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique,
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getAccount() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      // throw error;
+      console.log("Error getting account", error);
+    }
+    return null;
+  }
+
+  async logoutUser() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Error in logout", error);
+    }
+    return null;
+  }
+}
+
+const authService = new AuthService();
+export default authService;
